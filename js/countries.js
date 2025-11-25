5f98e750b2901337b377c3e888550e8d0d072ee3
Arão Sassa <192287522+AraoSassa26@users.noreply.github.com>
2025-11-25 10:26:39 +0000

    Add files via upload

---
+++ js/countries.js
@@ -0,0 +1,26 @@
+const paisesLusofonos = {
+    "Angola": [
+        "Luanda", "Benguela", "Lubango", "Huambo", "Malanje", "Cabinda", "Uíge"
+    ],
+    "Brasil": [
+        "São Paulo", "Rio de Janeiro", "Brasília", "Salvador", "Fortaleza", "Recife"
+    ],
+    "Portugal": [
+        "Lisboa", "Porto", "Braga", "Coimbra", "Faro", "Aveiro"
+    ],
+    "Moçambique": [
+        "Maputo", "Beira", "Nampula", "Tete", "Chimoio"
+    ],
+    "Cabo Verde": [
+        "Praia", "Mindelo", "Santa Catarina"
+    ],
+    "Guiné-Bissau": [
+        "Bissau", "Cacheu", "Gabu"
+    ],
+    "São Tomé e Príncipe": [
+        "São Tomé", "Santo António"
+    ],
+    "Timor-Leste": [
+        "Díli", "Baucau", "Same"
+    ]
+};
